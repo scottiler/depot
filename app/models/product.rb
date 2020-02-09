a35940c6012f272c9eba7bca10c18274411b82a0
Perry Iler <piler1@my.westga.edu>
2020-02-09 14:48:59 -0500

    Completed Iteration B2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than__or_equal_to: 0.01 }
-  validates ​:title​, ​uniqueness: ​​true
-  validates ​:image_url​, ​allow_blank: ​​true​, ​format: ​{ with: ​​%r{​​\.​​(gif|jpg|png)​​\Z​​}i​,
-                              message: ​​'must be a URL for GIF, JPG or PNG image.'}        
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: { with: %r(\.(gif|jpg|pug)\Z)i, message: 'must be a url for GIF, JPG or png image.' }
 end
